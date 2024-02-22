66fc7412b4c67240034d39505e3bdc4710131f3d
ivancotacte <cotactearmenion@gmail.com>
2024-02-22 13:03:45 +0800

    Added Some code!

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
